00f7c2c94c57888593e0d0918e40523037d2f256
Stephen Wasilewski <stephanwaz@gmail.com>
2020-09-17 16:03:13 +0200

    Bump version: 0.1.0 → 0.1.1

--- raytraverse/__init__.py
+++ raytraverse/__init__.py
@@ -10,7 +10,7 @@
 
 __author__ = """Stephen Wasilewski"""
 __email__ = 'stephanwaz@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['io', 'skycalc', 'translate', 'draw', 'metric', 'craytraverse',
            'lightfield', 'integrator', 'mapper', 'sampler', 'scene', 'renderer']
 
--- setup.py
+++ setup.py
@@ -108,6 +108,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.enterpriselab.ch/lightfields/raytraverse',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
     )
