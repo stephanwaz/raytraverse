a9899e2c24396db761df491d1fb1c6c9fcc8c75a
Stephen Wasilewski <stephanwaz@gmail.com>
2020-09-17 17:56:05 +0200

    try readthedocs with pip

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://gitlab.enterpriselab.ch/lightfields/raytraverse',
+    url='https://github.com/stephanwaz/raytraverse',
     version='0.1.1',
     zip_safe=False,
     )
