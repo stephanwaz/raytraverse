f4a4f182b5c49f05592e47ccc51cbb81eebf6432
Stephen Wasilewski <stephanwaz@gmail.com>
2020-09-18 11:09:50 +0200

    debug compilation

--- src/pybind11_add_module_libs.cmake
+++ src/pybind11_add_module_libs.cmake
@@ -36,8 +36,8 @@ function(pybind11_add_module_libs target_name)
   # py::module_local).  We force it on everything inside the `pybind11`
   # namespace; also turning it on for a pybind module compilation here avoids
   # potential warnings or issues from having mixed hidden/non-hidden types.
-  set_target_properties(${target_name} PROPERTIES CXX_VISIBILITY_PRESET "hidden"
-          CUDA_VISIBILITY_PRESET "hidden")
+#  set_target_properties(${target_name} PROPERTIES CXX_VISIBILITY_PRESET "hidden"
+#          CUDA_VISIBILITY_PRESET "hidden")
 
   if(ARG_NO_EXTRAS)
     return()
@@ -51,15 +51,15 @@ function(pybind11_add_module_libs target_name)
     endif()
   endif()
 
-  if(NOT MSVC AND NOT ${CMAKE_BUILD_TYPE} MATCHES Debug|RelWithDebInfo)
-    pybind11_strip(${target_name})
-  endif()
-
-  if(MSVC)
-    target_link_libraries(${target_name} PRIVATE pybind11::windows_extras)
-  endif()
-
-  if(ARG_OPT_SIZE)
-    target_link_libraries(${target_name} PRIVATE pybind11::opt_size)
-  endif()
+#  if(NOT MSVC AND NOT ${CMAKE_BUILD_TYPE} MATCHES Debug|RelWithDebInfo)
+#    pybind11_strip(${target_name})
+#  endif()
+#
+#  if(MSVC)
+#    target_link_libraries(${target_name} PRIVATE pybind11::windows_extras)
+#  endif()
+#
+#  if(ARG_OPT_SIZE)
+#    target_link_libraries(${target_name} PRIVATE pybind11::opt_size)
+#  endif()
 endfunction()
