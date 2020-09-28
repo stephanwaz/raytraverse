b0930952ffa12485481bafa232d762fd09d5c665
Stephen Wasilewski <stephanwaz@gmail.com>
2020-09-28 10:28:46 +0200

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup_dict = dict(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/stephanwaz/raytraverse',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
     )
 
