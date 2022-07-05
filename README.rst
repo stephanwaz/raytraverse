cbdfa61db180649711625ceab4db99d68fb74376
Stephen Wasilewski <stephanwaz@gmail.com>
2022-07-05 16:41:02 +0200

    Bump version: 1.3.4 → 1.3.5

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ===================
-raytraverse (1.3.4)
+raytraverse (1.3.5)
 ===================
 
 .. image:: https://img.shields.io/pypi/v/raytraverse?style=flat-square
--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/stephanwaz/raytraverse',
-    version='1.3.4',
+    version='1.3.5',
     zip_safe=False,
     )
